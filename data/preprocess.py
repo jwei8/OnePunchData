39918907d691d2bbf53868ff84f3bf9ed959d03d
Danny Hu <dhu2168@student.ubc.ca>
2023-11-15 11:55:42 -0800

    fix genres name

--- data/preprocess.py
+++ data/preprocess.py
@@ -66,4 +66,6 @@ print(top_1000_animes.info())
 print(top_1000_animes.describe(include='all'))
 print(genre_counts)
 
+top_1000_animes.rename(columns={'Genres':'Genre'}, inplace=True)
+
 top_1000_animes.to_csv('anime_processed.csv', index=False)
