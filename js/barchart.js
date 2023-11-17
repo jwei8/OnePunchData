db8fa663973843255154681f401d932eddaf967a
Wonhak Choi <wonhakchoi@gmail.com>
2023-11-17 13:38:02 -0800

    draw axis

---
+++ js/barchart.js
@@ -0,0 +1,129 @@
+class Barchart {
+
+    constructor(_config, _data) {
+        this.config = {
+            parentElement: _config.parentElement,
+            containerWidth: 500,
+            containerHeight: 260,
+            margin: {
+                top: 30,
+                right: 5,
+                bottom: 20,
+                left: 30
+            },
+            // Todo: Add or remove attributes from config as needed
+            tooltipPadding: 10, // Added a tooltip padding configuration
+        }
+        //   this.dispatcher = _dispatcher;
+        this.data = _data;
+        this.initVis();
+    }
+
+    initVis() {
+        let vis = this;
+
+        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+
+        vis.xScale = d3.scaleBand()
+            .range([0, vis.width])
+            .paddingInner(0.2);
+
+        vis.yScale = d3.scaleLinear()
+            .range([0, vis.height]);
+
+        vis.xAxis = d3.axisBottom(vis.xScale)
+            .tickSizeOuter(0);
+
+        vis.yAxis = d3.axisLeft(vis.yScale)
+            // .ticks(6)
+            .tickSize(-vis.width)
+            .tickSizeOuter(0)
+            .tickFormat(d3.format('d')); // Display years as integers
+
+        // Define size of SVG drawing area
+        vis.svg = d3.select(vis.config.parentElement)
+            .attr('width', vis.config.containerWidth)
+            .attr('height', vis.config.containerHeight);
+
+        vis.chart = vis.svg.append('g')
+            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+
+        vis.xAxisG = vis.chart.append('g')
+            .attr('class', 'axis x-axis')
+            .attr('transform', `translate(0,${vis.height})`);
+
+        // Append y-axis group 
+        vis.yAxisG = vis.chart.append('g')
+            .attr('class', 'axis y-axis');
+
+        // Append axis title
+        vis.svg.append('text')
+            .attr('class', 'axis-title')
+            .attr('x', 0)
+            .attr('y', 10)
+            .text('View 2');
+    }
+
+    updateVis() {
+        let vis = this;
+
+        // Group the data by genre and year and calculate the count for each group
+        var nestedData = d3.group(data, d => d.Genre, d => d.YearReleased);
+
+        // Flatten the nested data structure
+        vis.flattenedData = [];
+        nestedData.forEach(function (genre, genreKey) {
+            genre.forEach(function (year, yearKey) {
+                vis.flattenedData.push({
+                    Genre: genreKey,
+                    Year: yearKey,
+                    Count: year.length
+                });
+            });
+        });
+        console.log(vis.flattenedData);
+
+        //
+        let groupByGenre = d3.rollups(vis.data, v => v.length, d => d.Genre);
+        let genreCount = groupByGenre.map(data => data[1]);
+
+        var minYear = d3.min(vis.data, d => d.YearReleased);
+        var maxYear = d3.max(vis.data, d => d.YearReleased);
+
+        vis.xScale.domain([minYear, maxYear]) // year
+        vis.yScale.domain(genreCount); // count of anime per genre
+
+        vis.transformedData = groupByGenre.map(([year, count]) => ({
+            year,
+            count,
+        }));
+        // console.log(vis.transformedData)
+
+        vis.renderVis();
+    }
+
+    renderVis() {
+        let vis = this;
+
+        const bars = vis.chart.selectAll('.bar')
+            .data(vis.flattenedData)
+            .join('rect')
+            .attr('class', 'bar')
+            .attr('width', vis.xScale.bandwidth())
+            .attr('height', d => vis.height - vis.yScale(d.Count))
+            .attr('x', d => vis.xScale(d.Year))
+            .attr('y', d => vis.yScale(d.Count))
+            .on("click", function (event, d) {
+                // Check if current category is active and toggle class
+                const isActive = d3.select(this).classed("active");
+
+                d3.selectAll(".bar.active").classed("active", false);
+                d3.select(this).classed("active", !isActive);
+            });
+
+        // Update axes
+        vis.xAxisG.call(vis.xAxis).call((g) => g.select(".domain").remove());
+        vis.yAxisG.call(vis.yAxis).call((g) => g.select(".domain").remove());
+    }
+}
--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@
  * Load data from CSV file
  */
 
-d3.csv('data/FILENAME.csv')
-  .then(data => {
 
-    // Todo: Preprocess data and show chart
+d3.csv('data/anime_processed.csv')
+  .then(_data => {
+    data = _data
+    data.forEach(d => {
+      // Extract the year from the "Premiered" column and convert to number
+      d.YearReleased = parseInt(d.Premiered.match(/\d+/)[0]);
+    });
+
+    barchart = new Barchart({ parentElement: '#bar-chart' }, data);
+    barchart.updateVis();
 
   })
   .catch(error => console.error(error));
-
