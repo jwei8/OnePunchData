035c3d14511cec9c85ab68a7754dabf2edd6462d
Wonhak Choi <wonhakchoi@gmail.com>
2023-11-17 16:17:28 -0800

    static stacked bar chart

--- js/barchart.js
+++ js/barchart.js
@@ -34,7 +34,7 @@ class Barchart {
             .range([vis.height, 0]);
 
         vis.xAxis = d3.axisBottom(vis.xScale)
-            .ticks(6)
+            // .ticks(6)
             .tickSizeOuter(0);
 
         vis.yAxis = d3.axisLeft(vis.yScale)
@@ -65,32 +65,102 @@ class Barchart {
             .attr('x', 0)
             .attr('y', 24)
             .text('View 2');
+
+        // Initialize stack generator and specify the categories or layers
+        // that we want to show in the chart
+        vis.stack = d3.stack()
+            .keys(['Action', 'Sci-Fi', 'Drama', 'Slice of Life', 'Mystery', 'Comedy', 'Adventure', 'Game', 'Music', 'Harem']);
     }
+
 
     updateVis() {
         let vis = this;
 
         // Group the data by genre and year and calculate the count for each group
-        var nestedData = d3.group(data, d => d.Genre, d => d.YearReleased);
+        var nestedData = d3.group(data, d => d.YearReleased, d => d.Genre);
+        // console.log(nestedData)
 
         // Flatten the nested data structure
         vis.flattenedData = [];
-        nestedData.forEach(function (genre, genreKey) {
-            genre.forEach(function (year, yearKey) {
-                vis.flattenedData.push({
-                    Genre: genreKey,
-                    Year: yearKey,
-                    Count: year.length
-                });
+        // nestedData.forEach(function (genre, genreKey) {
+        //     genre.forEach(function (year, yearKey) {
+        //         vis.flattenedData.push({
+        //             Year: genreKey,
+        //             Genre: yearKey,
+        //             Count: year.length
+        //         });
+        //     });
+        // });
+        nestedData.forEach(function (year, yearKey) {
+            let groupedByYearObj = {};
+            groupedByYearObj['Year'] = yearKey;
+            groupedByYearObj["Action"] = 0;
+            groupedByYearObj["Sci-Fi"] = 0;
+            groupedByYearObj["Drama"] = 0;
+            groupedByYearObj["Slice of Life"] = 0;
+            groupedByYearObj["Mystery"] = 0;
+            groupedByYearObj["Comedy"] = 0;
+            groupedByYearObj["Adventure"] = 0;
+            groupedByYearObj["Game"] = 0;
+            groupedByYearObj["Music"] = 0;
+            groupedByYearObj["Harem"] = 0;
+            // console.log(groupedByYearObj);
+            year.forEach(function (genre, genreKey) {
+                // var genreCount = genre.length;
+                groupedByYearObj[genreKey] = genre.length;
+                // vis.flattenedData.push({
+                //     Genre: genreKey,
+                //     Year: yearKey,
+                //     Count: genre.length
+                // });
             });
+            vis.flattenedData.push(groupedByYearObj);
         });
+
+        // Sort the array by year
+        vis.flattenedData.sort(function (a, b) {
+            return a.Year - b.Year;
+        });
+
+
         console.log(vis.flattenedData);
+
+        // i.e. [{ key: 'easy', count: 10 }, {key: 'intermediate', ...
+        // const aggregatedDataMap = d3.rollups(vis.data, v => v.length, d => d.YearReleased);
+        // const aggregatedDataMap = d3.rollups(vis.data, v => v.length, d => d.Genre);
+        // console.log(aggregatedDataMap);
+
+        // Example data
+        // const data = [{
+        //     'year': 2015,
+        //     'milk': 10,
+        //     'water': 4
+        // },
+        // {
+        //     'year': 2016,
+        //     'milk': 12,
+        //     'water': 6
+        // },
+        // {
+        //     'year': 2017,
+        //     'milk': 6,
+        //     'water': 7
+        // }
+        // ];
+        // for each year -> Action: count, Comedt: 4
 
         // Set the scale input domains
         vis.xScale.domain(vis.flattenedData.map(d => d.Year));
-        vis.yScale.domain([0, d3.max(vis.flattenedData, d => d.Count)]);
+        vis.yScale.domain([0, d3.max(vis.flattenedData, d => d.Action + d['Sci-Fi'] + d.Drama + d['Slice of Life'] + d.Mystery + d.Comedy + d.Adventure + d.Game + d.Music + d.Harem)]);
+        // vis.yScale.domain([0, d3.max(vis.flattenedData, d => d.Count)]);
+
+        // Call stack generator on the dataset
+        vis.stackedData = vis.stack(vis.flattenedData);
+        console.log(vis.stackedData);
+
 
         // let groupByGenre = d3.rollups(vis.data, v => v.length, d => d.Genre);
+        // console.log(groupByGenre);
         // let genreCount = groupByGenre.map(data => data[1]);
 
         // var minYear = d3.min(vis.data, d => d.YearReleased);
@@ -112,21 +182,34 @@ class Barchart {
     renderVis() {
         let vis = this;
 
-        const bars = vis.chart.selectAll('.bar')
-            .data(vis.flattenedData)
+        const bars = vis.chart.selectAll('.category')
+            .data(vis.stackedData)
+            .join('g')
+            .attr('class', d => `category cat-${d.key}`)
+            .selectAll('rect')
+            .data(d => d)
             .join('rect')
-            .attr('class', 'bar')
-            .attr('width', vis.xScale.bandwidth())
-            .attr('height', d => vis.height - vis.yScale(d.Count))
-            .attr('x', d => vis.xScale(d.Year))
-            .attr('y', d => vis.yScale(d.Count))
-        // .on("click", function (event, d) {
-        //     // Check if current category is active and toggle class
-        //     const isActive = d3.select(this).classed("active");
+            .attr('x', d => vis.xScale(d.data.Year))
+            .attr('y', d => vis.yScale(d[1]))
+            .attr('height', d => vis.yScale(d[0]) - vis.yScale(d[1]))
+            .attr('width', vis.xScale.bandwidth());
 
-        //     d3.selectAll(".bar.active").classed("active", false);
-        //     d3.select(this).classed("active", !isActive);
-        // });
+
+        // const bars = vis.chart.selectAll('.bar')
+        //     .data(vis.flattenedData)
+        //     .join('rect')
+        //     .attr('class', 'bar')
+        //     .attr('width', vis.xScale.bandwidth())
+        //     .attr('height', d => vis.height - vis.yScale(d.Action))
+        //     .attr('x', d => vis.xScale(d.Year))
+        //     .attr('y', d => vis.yScale(d.Action))
+        //     .on("click", function (event, d) {
+        //         // Check if current category is active and toggle class
+        //         const isActive = d3.select(this).classed("active");
+
+        //         d3.selectAll(".bar.active").classed("active", false);
+        //         d3.select(this).classed("active", !isActive);
+        //     });
 
         // Update axes
         vis.xAxisG.call(vis.xAxis).call((g) => g.select(".domain").remove());
